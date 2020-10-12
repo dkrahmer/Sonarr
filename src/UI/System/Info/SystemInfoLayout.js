8869b3b3d7a94ff0c9d0a6f848988c8479461d67
Doug Krahmer <doug.git@remhark.com>
2020-10-12 12:26:36 -0700

    Remove "Disk Space" section on Status screen. Prevents waking up hard drives that may be sleeping.

--- src/UI/System/Info/SystemInfoLayout.js
+++ src/UI/System/Info/SystemInfoLayout.js
@@ -1,7 +1,6 @@
 var Backbone = require('backbone');
 var Marionette = require('marionette');
 var AboutView = require('./About/AboutView');
-var DiskSpaceLayout = require('./DiskSpace/DiskSpaceLayout');
 var HealthLayout = require('./Health/HealthLayout');
 var MoreInfoView = require('./MoreInfo/MoreInfoView');
 
@@ -10,14 +9,12 @@ module.exports = Marionette.Layout.extend({
 
     regions : {
         about     : '#about',
-        diskSpace : '#diskspace',
         health    : '#health',
         moreInfo  : '#more-info'
     },
 
     onRender : function() {
         this.health.show(new HealthLayout());
-        this.diskSpace.show(new DiskSpaceLayout());
         this.about.show(new AboutView());
         this.moreInfo.show(new MoreInfoView());
     }
